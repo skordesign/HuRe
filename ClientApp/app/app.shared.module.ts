bc7bf950fb5301f1f9a10207917050922822a819
skordesign <skordesign@outlook.com>
2017-12-09 09:45:10 +0700

    add nav

--- ClientApp/app/app.shared.module.ts
+++ ClientApp/app/app.shared.module.ts
@@ -5,28 +5,19 @@ import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './components/app/app.component';
-import { NavMenuComponent } from './components/navmenu/navmenu.component';
-import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
+import { CoreModule } from './modules/core/core.module';
 
 @NgModule({
     declarations: [
-        AppComponent,
-        NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
-        HomeComponent
+        AppComponent
     ],
     imports: [
         CommonModule,
+        CoreModule,
         HttpModule,
         FormsModule,
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
             { path: '**', redirectTo: 'home' }
         ])
     ]
---
+++ ClientApp/app/modules/core/core.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { NavComponent } from './nav/nav.component';
+
+@NgModule({
+    imports: [],
+    declarations: [NavComponent],
+    exports: [NavComponent]
+})
+export class CoreModule {
+
+}
+
