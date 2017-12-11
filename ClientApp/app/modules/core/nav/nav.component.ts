9c7c50220374bc0386de793bbb7be4ab6ee2ae9e
minhdoan96 <minhdoan.zaphkiel@gmail.com>
2017-12-11 19:46:37 +0700

    test function

--- ClientApp/app/modules/core/nav/nav.component.ts
+++ ClientApp/app/modules/core/nav/nav.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'hure-nav',
     templateUrl:'./nav.component.html',
     styleUrls: ['./nav.component.css']
 })
-export class NavComponent{
+export class NavComponent implements OnInit{
+    ngOnInit(): void {
+       //hàm này sễ khởi tạo khi gọi component này 
+        //viết kiểu này là typescript  , typescript là phiên bản nâng cao của jas
+    //    alert('test')
+    }
+    private test(){
+       alert('test')
+       
+    }
 
 }
