03a13b394d4b31afe3a3224037ccc58ae1f4838c
QuanVo <quanvo.dev@gmail.com>
2017-12-16 13:33:39 +0700

    edit

--- ClientApp/app/app.shared.module.ts
+++ ClientApp/app/app.shared.module.ts
@@ -6,12 +6,12 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './components/app/app.component';
 import { CoreModule } from './modules/core/core.module';
 import { DirectiveModule } from './modules/common/directives/directive.module';
-<<<<<<< HEAD
 import { HomeComponent } from "./components/home/home.component";
 import { routing } from "./app.router";
 import { IntroduceComponent } from "./components/introduce/introduce.component";
 import { UIModule } from './modules/common/ui/ui.module';
 import { ContactComponent } from "./components/contact/contact.component";
+import { ServiceModule } from "./modules/common/services/service.module";
 
 @NgModule({
     declarations: [
@@ -19,14 +19,6 @@ import { ContactComponent } from "./components/contact/contact.component";
         HomeComponent,
         IntroduceComponent,
         ContactComponent
-=======
-import { ServiceModule } from './modules/common/services/service.module';
-import { UIModule } from './modules/common/ui/ui.module';
-
-@NgModule({
-    declarations: [
-        AppComponent
->>>>>>> cd5300c79ef1054f7afc67a3a48420105d6cb975
     ],
     imports: [ 
         CommonModule,
--- ClientApp/app/components/home/home.component.ts
+++ ClientApp/app/components/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Http } from "@angular/http";
-
+import { ConfirmService } from '../../modules/common/services/frontend/confirm.service';
+import { AlertService } from '../../modules/common/services/frontend/alert.service';
 @Component({
     selector: 'home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-    constructor(private _httpService: Http) { }
+    constructor(private _httpService: Http,private confirmSvc: ConfirmService, private alertSvc:AlertService) { }
     apiValues: string[] = [];
     ngOnInit() {
         this._httpService.get('/api/values').subscribe(values => {
@@ -15,5 +16,15 @@ export class HomeComponent implements OnInit {
             this.apiValues = values.json() as string[];
             console.log(this.apiValues);
         });
+    }
+
+    public showDlg() {
+        this.confirmSvc.showConfirm("Help", "Me", [{
+            text: "OK", func: () => { console.log("OK") }
+        }])
+    }
+    public showAlert() {
+        this.alertSvc.showInfo("Hello");
+        this.alertSvc.showError("Hello");
     }
 }
