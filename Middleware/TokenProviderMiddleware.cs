9f27577da1b0ac0d653bc9165c2734a62051d42e
QuanVo <quanvo.dev@gmail.com>
2017-12-17 15:30:20 +0700

    sửa model repo

--- Middleware/TokenProviderMiddleware.cs
+++ Middleware/TokenProviderMiddleware.cs
@@ -49,85 +49,85 @@ namespace HuRe.Middleware
 
         private async Task GenerateToken(HttpContext context)
         {
-            var username = context.Request.Form["username"];
-            var password = context.Request.Form["password"];
+            //var username = context.Request.Form["username"];
+            //var password = context.Request.Form["password"];
 
-            var identity = await _options.IdentityResolver(username, password);
-            if (identity == null)
-            {
-                context.Response.StatusCode = 400;
-                await context.Response.WriteAsync("Invalid username or password.");
-                return;
-            }
+            //var identity = await _options.IdentityResolver(username, password);
+            //if (identity == null)
+            //{
+            //    context.Response.StatusCode = 400;
+            //    await context.Response.WriteAsync("Invalid username or password.");
+            //    return;
+            //}
 
-            var now = DateTime.UtcNow;
+            //var now = DateTime.UtcNow;
 
-            // Specifically add the jti (nonce), iat (issued timestamp), and sub (subject/user) claims.
-            // You can add other claims here, if you want:
-            var claims = new Claim[]
-            {
-                new Claim(JwtRegisteredClaimNames.Sub, username),
-                new Claim(JwtRegisteredClaimNames.Jti, await _options.NonceGenerator()),
-                new Claim(JwtRegisteredClaimNames.Iat, new DateTimeOffset(now).ToUniversalTime().ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer64)
-            };
+            //// Specifically add the jti (nonce), iat (issued timestamp), and sub (subject/user) claims.
+            //// You can add other claims here, if you want:
+            //var claims = new Claim[]
+            //{
+            //    new Claim(JwtRegisteredClaimNames.Sub, username),
+            //    new Claim(JwtRegisteredClaimNames.Jti, await _options.NonceGenerator()),
+            //    new Claim(JwtRegisteredClaimNames.Iat, new DateTimeOffset(now).ToUniversalTime().ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer64)
+            //};
 
-            // Create the JWT and write it to a string
-            var jwt = new JwtSecurityToken(
-                issuer: _options.Issuer,
-                audience: _options.Audience,
-                claims: claims,
-                notBefore: now,
-                expires: now.Add(_options.Expiration),
-                signingCredentials: _options.SigningCredentials);
-            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+            //// Create the JWT and write it to a string
+            //var jwt = new JwtSecurityToken(
+            //    issuer: _options.Issuer,
+            //    audience: _options.Audience,
+            //    claims: claims,
+            //    notBefore: now,
+            //    expires: now.Add(_options.Expiration),
+            //    signingCredentials: _options.SigningCredentials);
+            //var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
 
-            var response = new
-            {
-                access_token = encodedJwt,
-                expires_in = (int)_options.Expiration.TotalSeconds
-            };
+            //var response = new
+            //{
+            //    access_token = encodedJwt,
+            //    expires_in = (int)_options.Expiration.TotalSeconds
+            //};
 
-            // Serialize and return the response
-            context.Response.ContentType = "application/json";
-            await context.Response.WriteAsync(JsonConvert.SerializeObject(response, _serializerSettings));
+            //// Serialize and return the response
+            //context.Response.ContentType = "application/json";
+            //await context.Response.WriteAsync(JsonConvert.SerializeObject(response, _serializerSettings));
         }
 
         private static void ThrowIfInvalidOptions(TokenProviderOptions options)
         {
-            if (string.IsNullOrEmpty(options.Path))
-            {
-                throw new ArgumentNullException(nameof(TokenProviderOptions.Path));
-            }
+            //if (string.IsNullOrEmpty(options.Path))
+            //{
+            //    throw new ArgumentNullException(nameof(TokenProviderOptions.Path));
+            //}
 
-            if (string.IsNullOrEmpty(options.Issuer))
-            {
-                throw new ArgumentNullException(nameof(TokenProviderOptions.Issuer));
-            }
+            //if (string.IsNullOrEmpty(options.Issuer))
+            //{
+            //    throw new ArgumentNullException(nameof(TokenProviderOptions.Issuer));
+            //}
 
-            if (string.IsNullOrEmpty(options.Audience))
-            {
-                throw new ArgumentNullException(nameof(TokenProviderOptions.Audience));
-            }
+            //if (string.IsNullOrEmpty(options.Audience))
+            //{
+            //    throw new ArgumentNullException(nameof(TokenProviderOptions.Audience));
+            //}
 
-            if (options.Expiration == TimeSpan.Zero)
-            {
-                throw new ArgumentException("Must be a non-zero TimeSpan.", nameof(TokenProviderOptions.Expiration));
-            }
+            //if (options.Expiration == TimeSpan.Zero)
+            //{
+            //    throw new ArgumentException("Must be a non-zero TimeSpan.", nameof(TokenProviderOptions.Expiration));
+            //}
 
-            if (options.IdentityResolver == null)
-            {
-                throw new ArgumentNullException(nameof(TokenProviderOptions.IdentityResolver));
-            }
+            //if (options.IdentityResolver == null)
+            //{
+            //    throw new ArgumentNullException(nameof(TokenProviderOptions.IdentityResolver));
+            //}
 
-            if (options.SigningCredentials == null)
-            {
-                throw new ArgumentNullException(nameof(TokenProviderOptions.SigningCredentials));
-            }
+            //if (options.SigningCredentials == null)
+            //{
+            //    throw new ArgumentNullException(nameof(TokenProviderOptions.SigningCredentials));
+            //}
 
-            if (options.NonceGenerator == null)
-            {
-                throw new ArgumentNullException(nameof(TokenProviderOptions.NonceGenerator));
-            }
+            //if (options.NonceGenerator == null)
+            //{
+            //    throw new ArgumentNullException(nameof(TokenProviderOptions.NonceGenerator));
+            //}
         }
     }
 }
