e6490eb1f4666392a5fedc1b4f0eed06e70521c7
skordesign <skordesign@outlook.com>
2017-12-19 00:32:54 +0700

    optimize navbar

--- ClientApp/app/modules/home/home.module.ts
+++ ClientApp/app/modules/home/home.module.ts
@@ -4,11 +4,12 @@ import { HomeComponent } from './home.component';
 import { homeRouter } from './home.router';
 import { CoreModule } from '../core/core.module';
 import { UIModule } from '../common/ui/ui.module';
+import { JobsContainerComponent } from 'app/modules/home/jobs-container/jobs.-container.component';
 
 @NgModule({
     imports: [homeRouter, CoreModule, UIModule],
     exports: [],
-    declarations: [HomeComponent],
+    declarations: [HomeComponent, JobsContainerComponent],
     providers: [],
 })
 export class HomeModule { }
--- ClientApp/app/modules/home/home.router.ts
+++ ClientApp/app/modules/home/home.router.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { RouterModule } from '@angular/router';
+import { JobsContainerComponent } from 'app/modules/home/jobs-container/jobs.-container.component';
 
 const router: Routes = [
     {
