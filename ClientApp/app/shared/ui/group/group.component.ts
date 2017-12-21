0454f0b44aaef8397aac49602e39d6b24f618262
nonormal <32037127+nonormal@users.noreply.github.com>
2017-12-21 20:05:53 +0700

    binding-Group-Blog-Job

--- ClientApp/app/shared/ui/group/group.component.ts
+++ ClientApp/app/shared/ui/group/group.component.ts
@@ -1,9 +1,11 @@
-import{Component} from '@angular/core';
+import{Component,Input} from '@angular/core';
 @Component({
     selector:'hure-group',
     templateUrl:'./group.component.html',
     styleUrls:['./group.component.scss']
 })
 export class GroupComponent{
-    
+    @Input() image: string;
+    @Input() link: string;
+    @Input() text: string;
 }
--- ClientApp/app/shared/ui/job/job.component.ts
+++ ClientApp/app/shared/ui/job/job.component.ts
@@ -1,9 +1,15 @@
-import{Component} from '@angular/core';
+import{Component,Input} from '@angular/core';
 @Component({
     selector:'hure-job',
     templateUrl:'./job.component.html',
     styleUrls:['./job.component.scss']
 })
 export class JobComponent{
-    
+    @Input() link: string;
+    @Input() image: string;
+    @Input() post: string;
+    @Input() location: string;
+    @Input() job: string;
+    @Input() linkjobtype: string;
+    @Input() jobtype: string;
 }
