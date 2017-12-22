d3ac8e03b96da5451ff4d01afa1c91dc7f1fa90d
nonormal <32037127+nonormal@users.noreply.github.com>
2017-12-22 18:15:54 +0700

    add-guard-sinhvien&doanhnghiep

--- ClientApp/app/shared/guards/guard.module.ts
+++ ClientApp/app/shared/guards/guard.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { AdminGuard } from '@guards/admin.guard';
 import { AuthGuard } from '@guards/auth.guard';
-
+import { SinhVienGuard} from '@guards/sinhvien.guard';
+import { DoanhNghiepGuard} from '@guards/doanhnghiep.guard';
 
 @NgModule({
     imports: [],
     exports: [],
     declarations: [],
-    providers: [AdminGuard,AuthGuard],
+    providers: [AdminGuard,AuthGuard,SinhVienGuard,DoanhNghiepGuard],
 })
 export class GuardModule { }
