e17096bcc5b90d6e0cde88d242da265b4dc67f00
QVM0161195 <quanvo.dev@gmail.com>
2017-12-22 09:54:40 +0700

    không thêm data mẫu

--- Db/InitDb.cs
+++ Db/InitDb.cs
@@ -11,25 +11,25 @@ namespace HuRe.Db
          public static void Init(JobDbContext _context)
         {
             _context.Database.EnsureCreated();
-            if (!_context.PhanQuyens.Any())
-            {
-                _context.PhanQuyens.Add(new Models.PhanQuyen
-                {
-                    Ten = "Admin",
-                    MoTa = "Dùng cho quản trị"
-                });
-                _context.SaveChanges();
-            }
-            if (!_context.TaiKhoans.Any())
-            {
-                _context.TaiKhoans.Add(new Models.TaiKhoan
-                {
-                    TenTaiKhoan = "quanvo",
-                    MatKhau = Protector.HashPassword("123456"),
-                    PhanQuyenId = _context.PhanQuyens.First(o => o.Ten == "Admin").Id
-                });
-                _context.SaveChanges();
-            }
+            //if (!_context.PhanQuyens.Any())
+            //{
+            //    _context.PhanQuyens.Add(new Models.PhanQuyen
+            //    {
+            //        Ten = "Admin",
+            //        MoTa = "Dùng cho quản trị"
+            //    });
+            //    _context.SaveChanges();
+            //}
+            //if (!_context.TaiKhoans.Any())
+            //{
+            //    _context.TaiKhoans.Add(new Models.TaiKhoan
+            //    {
+            //        TenTaiKhoan = "quanvo",
+            //        MatKhau = Protector.HashPassword("123456"),
+            //        PhanQuyenId = _context.PhanQuyens.First(o => o.Ten == "Admin").Id
+            //    });
+            //    _context.SaveChanges();
+            //}
         }
     }
 }
