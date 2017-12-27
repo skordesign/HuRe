00f2db7ad1b42493aa00f05fcd1b4df4e7b3f423
minhdoan96 <minhdoan.zaphkiel@gmail.com>
2017-12-28 05:42:12 +0700

    change list of events  into "eventlist" component, import it to CoreModule so eventpage and homepage can take this component

--- ClientApp/app/app/end-user/event/event.module.ts
+++ ClientApp/app/app/end-user/event/event.module.ts
@@ -8,11 +8,13 @@ import { SharedModule } from "@shared/shared.module";
 
 import { eventRouter } from "@app/end-user/event/event.router";
 import { EventPageComponent } from "@app/end-user/event/pages/event-page.component";
+import { EventBannerComponent } from "@app/end-user/event/eventbanner/eventbanner.component";
+// import { EventListComponent } from "@app/end-user/event/eventlist/eventlist.component";
 
 @NgModule({
     imports: [CommonModule, eventRouter, CoreModule,SharedModule],
     exports: [],
-    declarations: [EventComponent,EventPageComponent],
+    declarations: [EventComponent,EventPageComponent,EventBannerComponent],
     providers: [],
 })
 export class EventModule{
